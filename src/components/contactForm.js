3dee3d9ed42fc8400ca7b23f920f3feffe351fd0
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2022-05-17 07:57:37 -0400

    fix form

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -30,120 +30,89 @@ const ContactForm = () => {
   }
 
   return (
-      <article className="contact-form">
-        <h1>contact us today for a free estimate</h1>
-        <form
-          name="contact"
-          method="POST"
-          data-netlify-honeypot="bot-field"
-          data-netlify="true"
-          action="/success/"
-          onSubmit={handleSubmit}
-        >
-          <input type="hidden" name="form-name" value="contact" />
-          <p hidden>
-            <label>
-              Don’t fill this out if you’re human:{" "}
-              <input name="bot-field" onChange={handleChange} />
-            </label>
-          </p>
-          <div className="form-group">
-            <label className="label">
-              Name:
-              <input
-                type="text"
-                name="name"
-                onChange={handleChange}
-                required
-                className="form-control"
-              />
-            </label>
-            <label className="label">
-              Email:
-              <input
-                type="email"
-                name="email"
-                onChange={handleChange}
-                required
-                className="form-control"
-              />
-            </label>
-            <label className="label">
-              Phone Number:
-              <input
-                type="tel"
-                name="phone"
-                onChange={handleChange}
-                className="form-control"
-              />
-            </label>
-            <label className="label">
-              Street Address:
-              <input
-                type="text"
-                name="street-address"
-                onChange={handleChange}
-                required
-                className="form-control"
-              />
-            </label>
-            <label className="label">
-              Zip Code:
-              <input
-                type="text"
-                name="zipcode"
-                onChange={handleChange}
-                required
-                className="form-control"
-              />
-            </label>
-            <p className="label">Service (check all that apply):</p>
-            <label for="service1" className="check-label">
-              <input
-                type="checkbox"
-                name="service1"
-                id="service1"
-                value="commercial-paving"
-                className="form-control-check"
-              />
-              Commercial Paving &amp; Sealing
-            </label>
-            <label for="service2" className="check-label">
-              <input
-                type="checkbox"
-                name="service2"
-                id="service2"
-                value="residential-paving"
-                className="form-control-check"
-              />
-              Residential Paving &amp; Sealing
-            </label>
-            <label for="service3" className="check-label">
-              <input
-                type="checkbox"
-                name="service3"
-                id="service3"
-                value="drainage"
-                className="form-control-check"
-              />
-              Drainage
-            </label>
-            <label className="label">
-              Additional Information:
-              <textarea
-                name="message"
-                id="message"
-                rows="5"
-                onChange={handleChange}
-                className="form-control"
-              ></textarea>
-            </label>
-          </div>
-          <button type="submit" className="submit-btn btn">
-            submit here
-          </button>
-        </form>
-      </article>
+    <article className="contact-form">
+      <h1>contact us today for a free estimate</h1>
+      <form
+        name="contact"
+        method="POST"
+        data-netlify-honeypot="bot-field"
+        data-netlify="true"
+        action="/success/"
+        onSubmit={handleSubmit}
+      >
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
+          <label>
+            Don’t fill this out if you’re human:{" "}
+            <input name="bot-field" onChange={handleChange} />
+          </label>
+        </p>
+        <div className="form-group">
+          <label className="label">
+            Name:
+            <input
+              type="text"
+              name="name"
+              onChange={handleChange}
+              required
+              className="form-control"
+            />
+          </label>
+          <label className="label">
+            Email:
+            <input
+              type="email"
+              name="email"
+              onChange={handleChange}
+              required
+              className="form-control"
+            />
+          </label>
+          <label className="label">
+            Phone Number:
+            <input
+              type="tel"
+              name="phone"
+              onChange={handleChange}
+              className="form-control"
+            />
+          </label>
+          <label className="label">
+            Street Address:
+            <input
+              type="text"
+              name="street-address"
+              onChange={handleChange}
+              required
+              className="form-control"
+            />
+          </label>
+          <label className="label">
+            Zip Code:
+            <input
+              type="text"
+              name="zipcode"
+              onChange={handleChange}
+              required
+              className="form-control"
+            />
+          </label>
+          <label className="label">
+            How can we help you?
+            <textarea
+              name="message"
+              id="message"
+              rows="5"
+              onChange={handleChange}
+              className="form-control"
+            ></textarea>
+          </label>
+        </div>
+        <button type="submit" className="submit-btn btn">
+          submit here
+        </button>
+      </form>
+    </article>
   )
 }
 
